a2852a90c27bf06b26a942f6e09efc5bce597c42
MurtyShikhar <shikhar.murty@gmail.com>
2017-07-26 18:12:06 -0400

    Doesn't learn

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 class Config:
-	num_epochs = 1000
+	num_epochs = 10
 	batch_size = 32
-	lr = 1e-2
 	train_embeddings=False
 	max_gradient_norm=10.0
-	hidden_state_size=10
+	hidden_state_size=100
 	embedding_size=100
 	data_dir="data/squad"
 	vocab_path="data/squad/vocab.dat"
